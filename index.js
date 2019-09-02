6265aa65d2f8833a5197a29510833042ae5ed9da
Andreas Thomsen <mc-hauge@hotmail.com>
2019-09-02 16:32:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -130,7 +130,7 @@ instance.prototype.config_fields = function () {
 			id: 'info',
 			label: 'Information',
 			width: 12,
-			value: 'If you are using this plugin, then it would probably mean that you are using some software that has not been supported yet, and we would therefore really appreciate if you made a module request for it here: https://github.com/bitfocus/companion-module-requests/issues In that way, more people will get to benefit from this in the future, thanks.'
+			value: '<strong>If you are using this plugin</strong>, then it would probably mean that you are using some software that has not been supported yet, and we would therefore really appreciate if you went ahead and made a module request for it here:<br /><br />https://github.com/bitfocus/companion-module-requests/issues<br /><br />In that way, more people will get to benefit from this in the future, thanks.'	
 		},
 		{
 			type: 'textinput',
